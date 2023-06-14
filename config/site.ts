f67e6ab9a7e7b80d103515bf4aa8c5f287a8f292
Mauro Alvarez <contact@mauroalvarez.com>
2023-06-14 13:04:51 +0200

    Add base project components and configuration

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Time Zone Difference",
+  description:
+    "Tool to check the time difference between two or more cities/countries",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/morklash",
+    github: "https://github.com/alvarezmauro/time-zone-difference",
+  },
+};
