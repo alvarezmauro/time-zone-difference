213a51967842f9b20b5e4bbcb421cdbcdc84fe02
Mauro Alvarez <contact@mauroalvarez.com>
2023-06-23 19:56:01 +0200

    Remove unnecessary lint to home page

--- config/site.ts
+++ config/site.ts
@@ -4,12 +4,7 @@ export const siteConfig = {
   name: "Time Zone Difference",
   description:
     "Tool to check the time difference between two or more cities/countries",
-  mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
-  ],
+  mainNav: [],
   links: {
     twitter: "https://twitter.com/morklash",
     github: "https://github.com/alvarezmauro/time-zone-difference",
