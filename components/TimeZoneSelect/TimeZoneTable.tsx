9a5f21db32fde91b8aaba448d615c5acdd6d2c4f
Mauro Alvarez <contact@mauroalvarez.com>
2023-06-30 01:46:57 +0200

    Add column menu

--- components/TimeZoneSelect/TimeZoneTable.tsx
+++ components/TimeZoneSelect/TimeZoneTable.tsx
@@ -2,6 +2,7 @@
 
 import { type ITimezoneOption } from "react-timezone-select";
 
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -10,6 +11,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Icons } from "@/components/icons";
 
 function getHourValue(hour: number, clockType: "12" | "24"): string {
   let hourValue = hour % 24;
@@ -44,13 +46,41 @@ export default function TimeZoneTable({
       <TableHeader>
         <TableRow className="transition-none hover:bg-inherit">
           {timeZoneData.map((timeZone, index) => {
-            let className = "border-x";
+            let addBorder = "border-x";
             if (index === timeZoneData.length - 1 || index === 0) {
-              className = "";
+              addBorder = "";
             }
             return (
-              <TableHead key={`${timeZone.value}_title`} className={className}>
-                {timeZone.altName}
+              <TableHead
+                key={`${timeZone.value}_title`}
+                className={`min-w-[12rem] px-0 ${addBorder}`}
+              >
+                <div className="flex h-full flex-col flex-nowrap justify-normal">
+                  <div className="flex shrink grow items-center justify-center px-4 pb-2 text-center">
+                    {timeZone.altName}
+                  </div>
+
+                  <div className="flex h-auto shrink grow-0 items-center rounded-md border-0 bg-background p-0">
+                    <Button
+                      variant="outline"
+                      className="m-0 flex flex-1 cursor-default select-none items-center rounded-none border-0 border-r-2 px-3 py-1.5 text-sm font-medium"
+                    >
+                      <Icons.chevronLeft className="h-4 w-4" />
+                    </Button>
+                    <Button
+                      variant="outline"
+                      className="m-0 flex flex-1 cursor-default select-none items-center rounded-none border-0 border-r-2 px-3 py-1.5 text-sm font-medium"
+                    >
+                      <Icons.x className="h-4 w-4" />
+                    </Button>
+                    <Button
+                      variant="outline"
+                      className="m-0 flex flex-1 cursor-default select-none items-center rounded-none border-0 px-3 py-1.5 text-sm font-medium"
+                    >
+                      <Icons.chevronRight className="h-4 w-4" />
+                    </Button>
+                  </div>
+                </div>
               </TableHead>
             );
           })}
@@ -65,7 +95,7 @@ export default function TimeZoneTable({
                 className = "";
               }
 
-              className += " min-w-[120px]";
+              className += " min-w-[10rem] text-center";
 
               const offset = timeZone.offset ?? 0;
 
