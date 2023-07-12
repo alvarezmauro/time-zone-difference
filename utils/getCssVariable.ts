342c7cb736cd072b31d8b0d30c7864297aab4454
Mauro Alvarez <contact@mauroalvarez.com>
2023-07-12 14:51:13 +0200

    Fix getCssVariable - Only try to get the CSS variables value when the window object exists

--- utils/getCssVariable.ts
+++ utils/getCssVariable.ts
@@ -1,3 +1,16 @@
+/**
+ * Get CSS variable value
+ * @param {string} variable - CSS variable name
+ * @returns {string} - CSS variable value
+ * @note This function will only be useful for client-side rendering components
+ * as we need to access the DOM to get the CSS variable value
+ */
 export default function getCssVariable(variable: string): string {
-  return getComputedStyle(document.documentElement).getPropertyValue(variable);
+  if (typeof window !== "undefined") {
+    return window
+      .getComputedStyle(document.documentElement)
+      .getPropertyValue(variable);
+  }
+
+  return "";
 }
